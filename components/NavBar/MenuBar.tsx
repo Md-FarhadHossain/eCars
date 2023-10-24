dbbc42fd7bb4de450abb60660f930e034a27e21a
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2023-10-25 04:03:54 +0600

    navbar is design is done with mobile responsive

---
+++ components/NavBar/MenuBar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Menu } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
+
+const MenuBar = () => {
+  const menus = [
+    { label: "All Cars" },
+    { label: "About Us" },
+    { label: "Blog" },
+    { label: "Contact" },
+  ];
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button
+          variant="outline"
+          className="lg:hidden md:block block md:mr-8 mr-4"
+        >
+          <Menu />
+        </Button>
+      </SheetTrigger>
+
+      {/* Shhet content */}
+      <SheetContent side="left">
+        <SheetHeader>
+          {/* Logo */}
+          <div className="text-3xl font-bold border-b pb-4">
+            <span className="text-[#00a419]">e</span>Cars
+          </div>
+        </SheetHeader>
+
+        <SheetDescription>
+          {/* Menu Links */}
+          <ul className="mt-4 text-lg">
+            {menus.map((menu) => (
+              <li className="ml-8 font-semibold py-4" key={menu.label}>
+                <Link href="/">{menu.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MenuBar;
---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,60 @@
+import { Search, Heart, UserCircle2 } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import MenuBar from "./MenuBar";
+
+const NavBar = () => {
+  const menus = [
+    { label: "All Cars" },
+    { label: "About Us" },
+    { label: "Blog" },
+    { label: "Contact" },
+  ];
+
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="flex justify-between items-center py-4 container">
+        {/* Nav right side */}
+        
+        <div className="flex items-center">
+          {/* Hamburger */}
+          
+
+          <MenuBar />
+          {/* Logo */}
+          <div className="text-3xl font-bold"><span className="text-[#00a419]">e</span>Cars</div>
+        
+
+          {/* Menu Links */}
+          <ul className="lg:flex md:hidden hidden">
+            {menus.map((menu) => (
+              <li className="ml-8 font-semibold" key={menu.label}>
+                <Link href="/">{menu.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Nav right side */}
+        <div className="flex items-center">
+          <ul className="flex items-center">
+            <li className="mr-4">
+              <Search />
+            </li>
+            <li className="mr-4 lg:block md:block hidden">
+              <Heart />
+            </li>
+            <li className="mr-8 lg:block md:block hidden">
+              <UserCircle2 />
+            </li>
+          </ul>
+
+          <Button variant="primary">Get a quote</Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
